4226cd0accbe5bb7afa4bd01f82f1c3a3460d809
Thamaraiselvan L <thamaraiselvan17292@gmail.com>
2023-02-26 16:08:22 +0530

    bc

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+
+provider "aws" {
+  region                  = "${var.region}"
+  access_key = "AKIAWA3S5VZAWKH52WWD"
+  secret_key = "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"
+  # shared_credentials_file = "~/.aws/credentials"
+  # profile                 = "development"
+}
+
